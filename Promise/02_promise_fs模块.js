c7f0d0ec511a4fdd7f1423dd4239e6057a08e4f6
shaoli <shaolisxxa@qq.com>
2021-01-11 16:14:54 +0800

    promise

---
+++ Promise/02_promise_fs模块.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+
+//回调函数形式
+// fs.readFile('Promise/resource/content.txt', (err, data) => {
+//     if (err) throw err
+//     console.log(data.toString())
+// })
+
+//promise形式
+let p = new Promise((resolve, reject) => {
+    fs.readFile('Promise/resource/content.txt', (err, data) => {
+        //如果失败
+        if (err) reject(err)
+        //如果成功
+        resolve(data)
+    })
+})
+
+// 调用then
+p.then(res => {
+    console.log(res.toString())
+}, err => {
+    console.log(err)
+})
