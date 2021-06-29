991c9813976d1781a52cd66d83d7d6965e6de394
shaoli <shaolisxxa@qq.com>
2021-06-29 08:51:05 +0800

    symbol

---
+++ Js基础/01_数据类型/基本数据类型_Symbol.js
@@ -0,0 +1,21 @@
+/**
+ * symbol 是一种基本数据类型 （primitive data type）。
+ * Symbol()函数会返回symbol类型的值，该类型具有静态属性和静态方法。
+ * 每个从Symbol()返回的symbol值都是唯一的。一个symbol值能作为对象属性的标识符；这是该数据类型仅有的目的。
+ */
+
+const symbol1 = Symbol();
+const symbol2 = Symbol(42);
+const symbol3 = Symbol('foo');
+
+console.log(typeof symbol1);
+// expected output: "symbol"
+
+console.log(symbol2 === 42);
+// expected output: false
+
+console.log(symbol3.toString());
+// expected output: "Symbol(foo)"
+
+console.log(Symbol('foo') === Symbol('foo'));
+ // expected output: false
---
+++ Js基础/01_数据类型/基本数据类型_Undefined.js
@@ -0,0 +1,10 @@
+/**
+ *
+ * Undefined（未定义）类型只有一个值，就是undefined
+ * 当声明一个变量，但是并不给变量赋值时，他的值就是undefined
+ * 
+ */
+
+let b;
+console.log(b); // undefined
+console.log(typeof b); // undefined
