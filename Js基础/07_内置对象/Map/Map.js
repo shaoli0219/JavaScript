5e1d529e71b1f59f533ee5d719319732d617a7f4
shaoli <shaolisxxa@qq.com>
2021-07-19 10:28:57 +0800

    Map

--- Js基础/07_内置对象/Map/Map.js
+++ Js基础/07_内置对象/Map/Map.js
@@ -7,21 +7,21 @@
  * Map.prototype.size
  *
  * Map 方法
- * Map.prototype.set(key, value) 设置Map对象中键的值。返回该Map对象。
+ * Map.prototype.set(key, value)    设置Map对象中键的值。返回该Map对象。
  *
- * Map.prototype.get(key) 返回键对应的值，如果不存在，则返回undefined。
+ * Map.prototype.get(key)           返回键对应的值，如果不存在，则返回undefined。
  *
- * Map.prototype.has(key) 返回一个布尔值，表示Map实例是否包含键对应的值。
+ * Map.prototype.has(key)           返回一个布尔值，表示Map实例是否包含键对应的值。
  *
- * Map.prototype.delete(key) 如果 Map 对象中存在该元素，则移除它并返回 true；否则如果该元素不存在则返回 false。
+ * Map.prototype.delete(key)        如果 Map 对象中存在该元素，则移除它并返回 true；否则如果该元素不存在则返回 false。
  *
- * Map.prototype.clear() 移除Map对象的所有键/值对 。
+ * Map.prototype.clear()            移除Map对象的所有键/值对 。
  *
- * Map.prototype.keys() keys()返回一个引用的Iterator对象。它包含按照顺序插入Map对象中每个元素的key值。
+ * Map.prototype.keys()             返回一个引用的Iterator对象。它包含按照顺序插入Map对象中每个元素的key值。
  *
  */
 
-
+// Map.prototype.keys()
 var myMap = new Map();
 myMap.set("0", "foo");
 myMap.set(1, "bar");
