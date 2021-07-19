544c9f7121b6c5d6ab4f77f5fa0c5e24dcf7464a
shaoli <shaolisxxa@qq.com>
2021-07-19 11:03:42 +0800

    set

--- Js基础/07_内置对象/Set/set.js
+++ Js基础/07_内置对象/Set/set.js
@@ -25,16 +25,16 @@
 const set1 = new Set([1, 2, '121', 'alice']);
 
 // Set.prototype.has()
-console.log(set1.has(1)); // expected output: true
-console.log(set1.has('121')); // expected output: true
-console.log(set1.has('a')); // expected output: false
+console.log(set1.has(1));       // expected output: true
+console.log(set1.has('121'));   // expected output: true
+console.log(set1.has('a'));     // expected output: false
 
 // Set.prototype.add()
 set1.add('Bob');
 
-//  Set.prototype.delete()
-console.log(set1.delete('bar')); // Returns false. No "bar" element found to be deleted.
-console.log(set1.delete(1)); // Returns true. Successfully removed.
+// Set.prototype.delete()
+console.log(set1.delete('bar'));    // Returns false. No "bar" element found to be deleted.
+console.log(set1.delete(1));        // Returns true. Successfully removed.
 
 // Set.prototype.size
 console.log(set1.size); // expected output: 5
