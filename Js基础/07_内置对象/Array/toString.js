f4b18275182b8d9212271479e7aa837efb444c78
shaoli <shaolisxxa@qq.com>
2021-08-02 10:25:38 +0800

    border-radius

--- Js基础/07_内置对象/Array/toString.js
+++ Js基础/07_内置对象/Array/toString.js
@@ -9,6 +9,6 @@ console.log(array1.toString());
 // expected output: "1,2,a,1a"
 
 const myHonda = { color: 'red', wheels: 4, engine: { cylinders: 4, size: 2.2 } };
-console.log(myHonda)
+console.log(myHonda);
 console.log(myHonda.toString());
 // expected output: "[object Object]"
