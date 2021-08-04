178a054d4bfea912ebad942ebfee48d4582423df
shaoli <shaolisxxa@qq.com>
2021-08-04 15:30:36 +0800

    slice

---
+++ Js基础/07_内置对象/String/slice.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ * String.prototype.slice()
+ * 
+ * 提取某个字符串的一部分，并返回一个新的字符串，且不会改动原字符串。
+ */
+var str1 = 'The morning is upon us.', // str1 的长度 length 是 23。
+    str2 = str1.slice(1, 8),
+    str3 = str1.slice(4, -2),
+    str4 = str1.slice(12),
+    str5 = str1.slice(30);
+console.log(str2); // 输出：he morn
+console.log(str3); // 输出：morning is upon u
+console.log(str4); // 输出：is upon us.
+console.log(str5); // 输出：""
