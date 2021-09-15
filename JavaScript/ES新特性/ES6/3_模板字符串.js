90cd4736efe16b5a72a373a1278bf9d133c7624d
shaoli <shaolisxxa@qq.com>
2021-09-15 16:55:14 +0800

    modify

--- JavaScript/ES新特性/ES6/3_模板字符串.js
+++ JavaScript/ES新特性/ES6/3_模板字符串.js
@@ -1,11 +1,15 @@
 // ES6中引入新的字符串声明方式`` （旧'' ""）
+
 //1. 声明
 let str = `新的字符串声明方式！！`;
 console.log(str, typeof str);
 
 //2.内容中可以直接出现换行符
-let oldStr = "<ul><li>老张</li><li>老王</li><li>老李</li></ul>"; //不可换行
-let newStr = `<ul>              //可以换行
+
+//不可换行
+let oldStr = "<ul><li>老张</li><li>老王</li><li>老李</li></ul>";
+//可以换行
+let newStr = `<ul>              
                 <li>老张</li>
                 <li>老王</li>
                 <li>老李</li>
