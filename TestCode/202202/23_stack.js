c43302c1921b683bfcd2c53c76858490a24c8ca9
shaoli <shaolisxxa@qq.com>
2022-02-23 16:02:21 +0800

    stack代码实现

--- TestCode/202202/23_stack.js
+++ TestCode/202202/23_stack.js
@@ -1,32 +1,39 @@
+// 栈结构
 class Stack {
     constructor() {
         this.items = []
     }
 
+    // push(item) 压栈操作，往栈里面添加元素
     push(item) {
         this.items.push(item)
     }
 
+    // pop() 出栈操作，从栈中取出元素，并返回取出的那个元素
     pop() {
         if (this.isEmpty())
             throw new Error('栈已空！')
-        this.items.pop()
+        return this.items.pop()
     }
 
+    // peek() 查看栈顶元素
     peek() {
         if (this.isEmpty())
             throw new Error('栈已空！')
         return this.items[this.items.length - 1]
     }
 
+    // isEmpty() 判断栈是否为空
     isEmpty() {
         return this.items.length === 0
     }
 
+    // size() 获取栈中元素个数
     size() {
         return this.items.length
     }
 
+    // toString() 返回以字符串形式的栈内元素数据
     toString() {
         return String(this.items)
     }
@@ -38,9 +45,11 @@ stack.push(2)
 stack.push(3)
 
 console.log(stack.peek())
+console.log(stack.size())
 console.log(stack.toString())
 
-stack.pop()
+console.log(stack.pop())
+console.log(stack.pop())
 stack.pop()
 stack.pop()
 console.log(stack.toString())
